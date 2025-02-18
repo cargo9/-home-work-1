559c809b6a87b65a2ccf8a94fcbb5114d541e98c
cargo9 <168007770+cargo9@users.noreply.github.com>
2025-02-18 20:54:44 +0200

    .

---
+++ script.js
@@ -0,0 +1,29 @@
+let age = 13;
+console.log(age);
+
+let name = "Yaroslav";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Yaroslav";
+console.log(myString);
+
+let myNumber = 1+10;
+console.log(myNumber);
+
+let message = "введіть своє імя";
+let userName = prompt(message);
+console.log(userName);
+
+alert(typeof userName);
+
+confirm(outMessage);
+let outMessage = "Ви дійсно хочете покинути сторінку?";
+console.log(userAnswer);
+
+let warning = "Увага! Дія може бути небезпечною." 
+alert(warning);
+let warningAnswer = "Ви впевнені, що хочете продовжити?" 
+alert(warningAnswer);
